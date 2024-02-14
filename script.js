921045ca30e3db1a5daedf2991ec13e972513aa4
DanielVNotice <92042221+DanielVNotice@users.noreply.github.com>
2024-02-14 02:10:51 +0000

    improving naming conventions

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-let now = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+$(document).ready(function(){
+    //To display current date and time
+    let currentTime = dayjs().format("dddd, MMMM D, YYYY h:mm A");
+    console.log(currentTime);
+    let displayDate = document.querySelector("#currentDay")
+    displayDate.textContent = currentTime ;
+})
 
-console.log(now);
 
-document.querySelector("#currentDay").textContent = now;
+
